093ddf24b820247154c5f72dac4138e29d0a6dc0
Martina Ursa <martina.ursa22@gmail.com>
2023-10-05 10:32:55 -0300

    Añadimos funcionalidad a botones para editar y borrar tareas

--- js/toDo.js
+++ js/toDo.js
@@ -1,55 +1,9 @@
 const inputBox = document.getElementById("input-box");
 const addTaskBtn = document.getElementById("add-task-btn");
+const taskList = document.getElementById("task-list");
 
-// Definimos array vacío
-const tasks = [];
-
-// Traemos al <ul> del HTML y le asignamos la clase 'list'
-let taskList = document.getElementById("task-list");
-
-function addNewTask() {
-    if (inputBox.value === '') {
-        Swal.fire({
-            icon: 'error',
-            title: 'Ups...',
-            text: 'Parece que no haz ingresado ninguna tarea.',
-        })
-    }
-    else { 
-        let newTask = inputBox.value;
-        tasks.push(newTask);
-        
-        const listItem = document.createElement('li');
-        listItem.classList.add('list-item')
-
-        listItem.innerHTML += `
-            <input type="checkbox" id="checkbox">
-            <label for="checkbox">${newTask}</label>
-            <div class="actions">
-                <button class="edit-btn"><i class="fa-regular fa-pen-to-square" style="color: #595959;"></i></button>
-                <button class="delete-btn"><i class="fa-regular fa-circle-xmark" style="color: #595959;"></i></button>
-            </div>`
-
-
-        // Escuchamos evento al hacer click en el botón delete-btn
-        const deleteButton = listItem.querySelector(".delete-btn");
-
-        deleteButton.addEventListener("click", () => {
-            listItem.remove();
-            
-            console.log(tasks);
-        })
-
-
-        taskList.appendChild(listItem)
-        inputBox.value = ''; 
-    
-
-        // Escuchamos evento al hacer 'click' en el botón '+'
-        addTaskBtn.addEventListener('click', addNewTask);
-    }
-    
-}
+// Escuchamos evento al hacer 'click' en el botón '+'
+addTaskBtn.addEventListener('click', addNewTask);
 
 // Escuchamos evento al dar Enter
 inputBox.addEventListener("keyup", (e) => {
@@ -58,4 +12,68 @@ inputBox.addEventListener("keyup", (e) => {
     }
     
 })
+
+
+// Función para añadir tareas
+function addNewTask() {
+    if (inputBox.value === '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Ups...',
+            text: 'Parece que no haz ingresado ninguna tarea.',
+            width: 400,
+            confirmButtonColor: '#1C4662',
+        })
+    }
+    else { 
+        let newTask = inputBox.value;
+        
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-item')
+
+        listItem.innerHTML += `
+            <input type="checkbox" id="checkbox">
+            <label class='label' for="checkbox">${newTask}</label>
+            <div class="actions">
+                <button class="edit-btn"><i class="fa-regular fa-pen-to-square" style="color: #595959;"></i></button>
+                <button class="delete-btn"><i class="fa-regular fa-circle-xmark" style="color: #595959;"></i></button>
+            </div>`
+
+        taskList.append(listItem)
+        inputBox.value = ''; 
+
+
+        // Escuchamos evento al hacer click en el botón delete-btn
+        const deleteButton = listItem.querySelector(".delete-btn");
+
+        deleteButton.addEventListener("click", () => {
+            listItem.remove();
+        })
+
+
+        // Escuchamos evento al hacer click en el botón edit-btn
+        const editButton = listItem.querySelector(".edit-btn");
+        
+        editButton.addEventListener("click", () => {
+            Swal.fire({
+                title: 'Ingresa una nueva tarea:',
+                input: 'text',
+                showCancelButton: true,
+                cancelButtonText: 'Cancelar',
+                confirmButtonText: 'Guardar',
+                confirmButtonColor:'#E3B029',
+                inputValidator: (value) => {
+                    if (!value) {
+                    return '¡Cuidado! Debes ingresar algo para continuar.';
+                    }
+                }
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    const newTaskValue = result.value; // result.value es el valor ingresado por el usuario en el prompt de Swal
+                    listItem.querySelector('.label').innerText = newTaskValue;
+                }
+            });
+        });
+    }
+}
 
