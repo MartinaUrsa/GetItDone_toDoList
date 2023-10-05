ba506eb68087224ea203b97fa99f5de266596951
Martina Ursa <martina.ursa22@gmail.com>
2023-10-05 08:42:43 -0300

    Añadimos escucha de eventos sobre lista de tareas

--- js/toDo.js
+++ js/toDo.js
@@ -7,43 +7,55 @@ const tasks = [];
 // Traemos al <ul> del HTML y le asignamos la clase 'list'
 let taskList = document.getElementById("task-list");
 
-
-// Indicamos qué ocurre al hacer 'click' en el botón '+'
-addTaskBtn.addEventListener('click', () => {
+function addNewTask() {
     if (inputBox.value === '') {
-        alert('¡Ups! Parece que no se ha ingresado ninguna nueva tarea.')
+        Swal.fire({
+            icon: 'error',
+            title: 'Ups...',
+            text: 'Parece que no haz ingresado ninguna tarea.',
+        })
     }
-
     else { 
         let newTask = inputBox.value;
         tasks.push(newTask);
-    
-        taskList.innerHTML += `
-            <li class="listItem">
-                <input type="checkbox" id="checkbox">
-                <label for="checkbox">${newTask}</label>
-            </li> `
+        
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-item')
 
+        listItem.innerHTML += `
+            <input type="checkbox" id="checkbox">
+            <label for="checkbox">${newTask}</label>
+            <div class="actions">
+                <button class="edit-btn"><i class="fa-regular fa-pen-to-square" style="color: #595959;"></i></button>
+                <button class="delete-btn"><i class="fa-regular fa-circle-xmark" style="color: #595959;"></i></button>
+            </div>`
+
+
+        // Escuchamos evento al hacer click en el botón delete-btn
+        const deleteButton = listItem.querySelector(".delete-btn");
+
+        deleteButton.addEventListener("click", () => {
+            listItem.remove();
+            
+            console.log(tasks);
+        })
+
+
+        taskList.appendChild(listItem)
         inputBox.value = ''; 
-    } 
-});
+    
 
+        // Escuchamos evento al hacer 'click' en el botón '+'
+        addTaskBtn.addEventListener('click', addNewTask);
+    }
+    
+}
 
-
-// Versión sin checkbox de la lista (funciona bien) 
-// if (inputBox.value === '') {
-//     alert('¡Ups! Parece que no se ha ingresado ninguna nueva tarea.')
-// }
-
-// else { 
-//     let newTask = inputBox.value;
-//     tasks.push(newTask);
-
-//     taskList.innerHTML += `<li class: listItem>${newTask}</li>`
-
-//     inputBox.value = ''; 
-//     } ;
-
-
-
+// Escuchamos evento al dar Enter
+inputBox.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        addNewTask();
+    }
+    
+})
 
