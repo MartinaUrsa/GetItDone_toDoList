efb02e40b447a51b31be51b6c54f93158c82e219
Martina Ursa <martina.ursa22@gmail.com>
2023-10-20 09:25:12 -0300

    Separamos funciones en archivo js aparte - corregimos localStorage para que guarde el estado de cada tarea ('completed' || 'active')

---
+++ js/functions.js
@@ -0,0 +1,147 @@
+// Añadir tareas
+function addNewTask() {
+    if (inputBox.value.trim().length !== 0) {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-item');
+
+        let newTask = {task: inputBox.value, status: 'active', id: Math.random()};
+
+        tasks.push(newTask);
+
+        listItem.innerHTML += `
+            <input type="checkbox" id="checkbox">
+            <input type="text" id="itemText" value="${newTask.task}" disabled>
+            <div class="actions">
+                <button class="edit-btn"><i class="fa-regular fa-pen-to-square" style="color: #595959;"></i></button>
+                <button class="delete-btn"><i class="fa-regular fa-circle-xmark" style="color: #595959;"></i></button>
+            </div>`
+
+        taskList.appendChild(listItem);
+        inputBox.value = '';
+
+        // Llamamos a la función saveTaskToStorage() para guardar el nuevo array con la newTask
+        saveTaskToStorage(tasks);
+
+        // Llamamos a función deleteTask() para que pueda acceder al listItem
+        deleteTask(listItem);
+
+        // Llamamos a función editTask() para que pueda acceder al listItem
+        editTask(listItem);
+
+        // Escuchamos evento 'change' en checkbox
+        completedTask(listItem);
+    }
+    else { 
+        Swal.fire({
+            icon: 'error',
+            title: 'Ups...',
+            text: 'Parece que no haz ingresado ninguna tarea.',
+            width: 400,
+            confirmButtonColor: '#1C4662',
+        })
+    };
+};
+
+
+// Completar tarea
+function completedTask(listItem) {
+    let checkbox = listItem.querySelector('#checkbox');
+    checkbox.addEventListener("change", () => {
+        if(checkbox.checked) {
+            listItem.classList.add("completed");
+            const itemText = listItem.querySelector('#itemText');
+            const taskIndex = tasks.findIndex(newTask => newTask.task === itemText.value);
+            if(taskIndex !== -1) {
+                tasks[taskIndex].status = 'completed';
+                saveTaskToStorage(tasks);
+            }
+        } else {
+            listItem.classList.remove("completed");
+            const itemText = listItem.querySelector('#itemText');
+            const taskIndex = tasks.findIndex(newTask => newTask.task === itemText.value);
+            if(taskIndex !== -1) {
+                tasks[taskIndex].status = 'active';
+                saveTaskToStorage(tasks);            
+            }
+        }
+    });
+}
+
+
+// Editar tarea
+function editTask(listItem) {
+    const editButton = listItem.querySelector(".edit-btn");
+    editButton.addEventListener("click", () => {
+        const itemText = listItem.querySelector('#itemText');
+        const taskIndex = tasks.indexOf(itemText.value);
+        itemText.removeAttribute("disabled");
+        itemText.focus();
+        itemText.setSelectionRange(itemText.value.length, itemText.value.length);
+
+        itemText.addEventListener("keyup", (e) => {
+            if (e.key === "Enter") {
+                if(itemText.value.length === 0) {
+                    listItem.remove();
+                    tasks.splice(taskIndex, 1);
+                    // Actualizamos localStorage
+                    saveTaskToStorage(tasks);
+
+                } else {
+                    itemText.setAttribute("disabled", "disabled");
+                    tasks[taskIndex] = itemText.value;
+                    // Actualizamos localStorage
+                    saveTaskToStorage(tasks);
+
+                    Toastify({
+                        text: 'Tarea editada con éxito.',
+                        duration: 3000,
+                        gravity: 'bottom',
+                        style: {
+                            background: 'linear-gradient(to right, #D0A95D, #E3B029)'
+                        }
+                    }).showToast()
+                }
+            }
+        })
+    });
+}
+
+
+// Eliminar tarea
+function deleteTask(listItem) {
+    const deleteButton = listItem.querySelector(".delete-btn");
+    deleteButton.addEventListener("click", () => {
+        Swal.fire({
+            icon: 'warning',
+            title: `¡Está por eliminar una tarea! ¿Avanzamos?`,
+            showCancelButton: true,
+            cancelButtonText: 'Cancelar',
+            confirmButtonText: 'Aceptar',
+            confirmButtonColor:'#E3B029',
+            width: 400,
+            }).then((result) => {
+            if (result.isConfirmed) {
+                // Eliminamos task de array tasks
+                const itemText = listItem.querySelector('#itemText');
+                const taskIndex = tasks.indexOf(itemText.value);
+                tasks.splice(taskIndex, 1);
+
+                // Eliminamos tasks de taskList
+                listItem.remove();
+
+                // Actualizamos localStorage
+                saveTaskToStorage(tasks);
+
+                Toastify({
+                    text: 'Tarea eliminada correctamente.',
+                    duration: 3000,
+                    gravity: 'bottom',
+                    style: {
+                        background: 'linear-gradient(to right, #D0A95D, #E3B029)'
+                    }
+                }).showToast();
+            }
+        });
+    });
+}
+
--- js/toDo.js
+++ js/toDo.js
@@ -19,55 +19,20 @@ inputBox.addEventListener("keyup", (e) => {
 document.addEventListener('DOMContentLoaded', getTasks);
 
 
-// FUNCIONES
-// Añadir tareas
-function addNewTask() {
-    if (inputBox.value === '') {
-        Swal.fire({
-            icon: 'error',
-            title: 'Ups...',
-            text: 'Parece que no haz ingresado ninguna tarea.',
-            width: 400,
-            confirmButtonColor: '#1C4662',
-        })
-    }
-    else { 
-        const listItem = document.createElement('li');
-        listItem.classList.add('list-item');
 
-        let newTask = inputBox.value;
+// LOCALSTORAGE
+// Guardar tareas en el localStorage
+function saveTaskToStorage(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
-        tasks.push(newTask);
+// Tomar las tareas del storage y parsearlas
+function getTaskStorage() {
+    const tasksStorage = JSON.parse(localStorage.getItem('tasks'));
+    return tasksStorage;
+}
 
-        listItem.innerHTML += `
-            <input type="checkbox" id="checkbox">
-            <input type="text" id="itemText" value="${newTask}" disabled>
-            <div class="actions">
-                <button class="edit-btn"><i class="fa-regular fa-pen-to-square" style="color: #595959;"></i></button>
-                <button class="delete-btn"><i class="fa-regular fa-circle-xmark" style="color: #595959;"></i></button>
-            </div>`
-
-        taskList.appendChild(listItem)
-        inputBox.value = '';
-
-        // Llamamos a la función saveTaskToStorage() para guardar el nuevo array con la newTask
-        saveTaskToStorage(tasks);
-
-        // Llamamos a función deleteTask() para que pueda acceder al listItem
-        deleteTask(listItem);
-
-        // Llamamos a función editTask() para que pueda acceder al listItem
-        editTask(listItem);
-
-        // Escuchamos evento 'change' en checkbox
-        const checkbox = listItem.querySelector('#checkbox');
-        checkbox.addEventListener("change", () => completedTask(listItem, checkbox));
-
-        // Guardamos tarea en storage
-        saveTaskToStorage(tasks);
-    };
-};
-
+// Mostrar tareas del storage en el DOM
 function showTasks(tasks) {
     taskList.innerHTML = '';
 
@@ -76,123 +41,32 @@ function showTasks(tasks) {
         listItem.classList.add('list-item');
         listItem.innerHTML += `
             <input type="checkbox" id="checkbox">
-            <input type="text" id="itemText" value="${newTask}" disabled>
+            <input type="text" id="itemText" value="${newTask.task}" disabled>
             <div class="actions">
                 <button class="edit-btn"><i class="fa-regular fa-pen-to-square" style="color: #595959;"></i></button>
                 <button class="delete-btn"><i class="fa-regular fa-circle-xmark" style="color: #595959;"></i></button>
             </div>`
-    
+        
+        let checkbox = listItem.querySelector('#checkbox');
+        if(newTask.status === 'completed') {
+            checkbox.checked = true;
+            listItem.classList.add("completed");
+        }
+        
         taskList.appendChild(listItem)
 
         // Aplicamos eventos a listItems tomados del localStorage 
         deleteTask(listItem);
         editTask(listItem);
+        completedTask(listItem);
 
-        const checkbox = listItem.querySelector('#checkbox');
-        checkbox.addEventListener("change", () => completedTask(listItem, checkbox));
-        
     });
 };
 
-// Guardar tareas en el localStorage
-function saveTaskToStorage(tasks) {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-function getTaskStorage() {
-    const tasksStorage = JSON.parse(localStorage.getItem('tasks'));
-    return tasksStorage;
-}
-
+// Verificar si hay tareas en el storage y llamar a la función showTasks()
 function getTasks() {
     if(localStorage.getItem('tasks')) {
         tasks = getTaskStorage();
         showTasks(tasks);
     };
 };
-
-// Eliminar tarea
-function deleteTask(listItem) {
-    const deleteButton = listItem.querySelector(".delete-btn");
-    deleteButton.addEventListener("click", () => {
-        Swal.fire({
-            icon: 'warning',
-            title: `¡Está por eliminar una tarea! ¿Avanzamos?`,
-            showCancelButton: true,
-            cancelButtonText: 'Cancelar',
-            confirmButtonText: 'Aceptar',
-            confirmButtonColor:'#E3B029',
-            width: 400,
-            }).then((result) => {
-            if (result.isConfirmed) {
-                // Eliminamos task de array tasks
-                const itemText = listItem.querySelector('#itemText');
-                const taskIndex = tasks.indexOf(itemText.value);
-                tasks.splice(taskIndex, 1);
-
-                // Eliminamos tasks de taskList
-                listItem.remove();
-
-                // Actualizamos localStorage
-                saveTaskToStorage(tasks);
-
-                Toastify({
-                    text: 'Tarea eliminada correctamente.',
-                    duration: 3000,
-                    gravity: 'bottom',
-                    style: {
-                        background: 'linear-gradient(to right, #D0A95D, #E3B029)'
-                    }
-                }).showToast();
-            }
-        });
-    });
-}
-
-// Editar tarea
-function editTask(listItem) {
-    const editButton = listItem.querySelector(".edit-btn");
-    editButton.addEventListener("click", () => {
-        const itemText = listItem.querySelector('#itemText');
-        const taskIndex = tasks.indexOf(itemText.value);
-        itemText.removeAttribute("disabled");
-        itemText.focus();
-        itemText.setSelectionRange(itemText.value.length, itemText.value.length);
-
-        itemText.addEventListener("keyup", (e) => {
-            if (e.key === "Enter") {
-                if(itemText.value.length === 0) {
-                    listItem.remove();
-                    tasks.splice(taskIndex, 1);
-                    // Actualizamos localStorage
-                    saveTaskToStorage(tasks);
-
-                } else {
-                    itemText.setAttribute("disabled", "disabled");
-                    tasks[taskIndex] = itemText.value;
-                    // Actualizamos localStorage
-                    saveTaskToStorage(tasks);
-
-                    Toastify({
-                        text: 'Tarea editada con éxito.',
-                        duration: 3000,
-                        gravity: 'bottom',
-                        style: {
-                            background: 'linear-gradient(to right, #D0A95D, #E3B029)'
-                        }
-                    }).showToast()
-                }
-            }
-        })
-    });
-}
-
-// Completar tarea
-function completedTask(listItem, checkbox) {
-    if(checkbox.checked) {
-        listItem.classList.add("completed")
-    } else {
-        listItem.classList.remove("completed")
-    }
-}
-
