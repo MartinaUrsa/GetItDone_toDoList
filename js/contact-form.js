159ef8932f8dcf4152ca5d64e556474603f42c96
Martina Ursa <martina.ursa22@gmail.com>
2023-10-26 10:18:15 -0300

    Incorporamos API (EmailJS) para envío de mails a través de formulario

--- js/contact-form.js
+++ js/contact-form.js
@@ -4,9 +4,60 @@ const userName = document.querySelector('#user_name');
 const userEmail = document.querySelector('#user_email');
 const message = document.querySelector('#message');
 
+// Evento submit al button del form
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(userName.value);
-    console.log(userEmail.value);
-    console.log(message.value);
-x})
+    
+    const body = {
+        service_id: 'service_bebm2km',
+        template_id: 'contact_form',
+        user_id: '0OII96_5hfFl7zTgU',
+        template_params: {
+            'user_name': userName.value,
+            'user_email': userEmail.value,
+            'message': message.value,
+        }
+    };
+
+    sendEmail(body)
+        .then((resp) => {
+            Swal.fire({
+                icon: 'success',
+                title: '¡Éxito!',
+                text: 'El formulario fue enviado correctamente.',
+                width: 400,
+                confirmButtonColor: '#1C4662',
+            });
+            return resp;
+        })
+        .catch((err) => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Ups!',
+                text: 'Parece que algo salió mal. Por favor, intenta nuevamente.',
+                width: 400,
+                confirmButtonColor: '#1C4662',
+            })
+            return err;
+        });
+
+    userName.value = '';
+    userEmail.value = '';
+    message.value = '';
+});
+
+
+// sendEmail function
+const sendEmail = async (body) => {
+    const config = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const response = await fetch('https://api.emailjs.com/api/v1.0/email/send', config)
+    const data = await response.text();
+    return data;
+}
